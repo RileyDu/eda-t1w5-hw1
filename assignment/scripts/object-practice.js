44d937f786d071322172c11081631e1acc10eaba
Riley DuBord <thelifeofdu@gmail.com>
2023-12-04 15:27:44 -0600

    formatting done. DCR.

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -50,5 +50,5 @@ console.log(me.shoeCount);
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
-me.favoriteColor = 'white';
+me.favoriteColor = "white";
 console.log(me.favoriteColor);
